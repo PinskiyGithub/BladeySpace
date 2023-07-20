5014306862d9784c0427ba5413717a65f5ae6b94
PinskiyGithub <110927886+PinskiyGithub@users.noreply.github.com>
2023-07-20 19:08:39 +0300

    Update Easter-EGG-NUMBER1.cs
    update 3

--- Easter-EGG-NUMBER1.cs
+++ Easter-EGG-NUMBER1.cs
@@ -1,2 +1,8 @@
 ﻿
 Console.WriteLine("I've always felt alone... my whole life, for as long as I can remember. I don't know if I like it... or if I'm just used to it, but I do know this: being lonely does things to you, and feeling shit and bitter and angry, all the time, just... eats away at you");
+Console.WriteLine("Улитка заходит в бар, но бармен заявляет: "У нас строгая политика в отношении улиток!" — и ногой выпихивает ее на улицу. Через неделю улитка возвращается в бар и говорит бармену: "Ну и нахуя ты это сделал!?"");
+Console.WriteLine("Шел медведь по лесу, видит — машина горит, ну он вызвал пожарных, её и потушили.");
+Console.WriteLine("Шел медведь по лесу, видит — машина горит, ну он вызвал пожарных, они приехали, оценили ситуацию, сели в машину и сгорели.");
+Console.WriteLine("Идёт медведь по лесу, видит — машина горит. Сел в нее и сгорел.");
+//хера я тут анекдотов понаставлял.жалко челика который захочет компилировать этот код 
+
