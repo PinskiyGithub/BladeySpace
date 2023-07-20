06ccb6870ef73c44ac7118652f6c667a8bfdbd1e
PinskiyGithub <110927886+PinskiyGithub@users.noreply.github.com>
2023-07-20 18:49:17 +0300

    update 0.0.3

---
+++ Easter-EGG-NUMBER1.cs
@@ -0,0 +1,2 @@
+﻿
+Console.WriteLine("I've always felt alone... my whole life, for as long as I can remember. I don't know if I like it... or if I'm just used to it, but I do know this: being lonely does things to you, and feeling shit and bitter and angry, all the time, just... eats away at you");
